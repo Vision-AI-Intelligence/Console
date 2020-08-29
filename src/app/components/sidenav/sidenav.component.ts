52d2ba31a1947952a8cb8cfb10a9aae625267d83
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-08-29 11:32:05 +0700

    split sidenav from toolbar

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  @Output() closeSidenav = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onToggleClose() {
+    this.closeSidenav.emit();
+  }
+}
