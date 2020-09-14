0f8a379d4ed7df439dee576b03cbeb8e9c4e6a9c
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-09-14 20:07:16 +0700

    Fix create project button

--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -1,46 +1,26 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-projectmanagement',
-  templateUrl: './projectmanagement.component.html',
-  styleUrls: ['./projectmanagement.component.scss']
+  selector: "app-projectmanagement",
+  templateUrl: "./projectmanagement.component.html",
+  styleUrls: ["./projectmanagement.component.scss"],
 })
 export class ProjectmanagementComponent implements OnInit {
+  @ViewChild("buttonsContainer", { static: true }) buttonsContainer: ElementRef;
 
-  @ViewChild('buttonsContainer', { static: true }) buttonsContainer: ElementRef;
+  constructor(private renderer: Renderer2) {}
 
-  constructor(private renderer: Renderer2) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-  buttons: {
-    id: number
-  }[] = [{ id: 0 }];
+  public projects = [];
 
   generateButton() {
-    const button = this.renderer.createElement('button');
-    const color = '#FF9237';
-    const font = 'Open Sans';
-    const width = '15vh';
-    const height = '15vh';
-    const borderRadius = '10px';
-    const border = '0';
-    const margin = '1%';
-    this.renderer.setStyle(button, 'background', color);
-    this.renderer.setStyle(button, 'font-family', font);
-    this.renderer.setStyle(button, 'width', width);
-    this.renderer.setStyle(button, 'height', height);
-    this.renderer.setStyle(button, 'border-radius', borderRadius);
-    this.renderer.setStyle(button, 'border', border);
-    this.renderer.setStyle(button, 'outline', border);
-    this.renderer.setStyle(button, 'margin', margin);
-    this.renderer.setAttribute(button, 'button', 'mat-flat-button');
-    const text = this.renderer.createText('New button');
-    this.renderer.appendChild(button, text);
-    this.buttons.push({
-      id: this.buttons.length
-    });
-    this.renderer.appendChild(this.buttonsContainer.nativeElement, button);
-    console.log(this.buttons.length);
+    this.projects.push({ name: "Project " + this.projects.length });
   }
 }
