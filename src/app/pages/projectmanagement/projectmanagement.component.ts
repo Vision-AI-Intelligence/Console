a5dcedad0906763cfffd8c0ec537cfdaa7a9b84d
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-09-14 21:35:11 +0700

    fixed something

--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -1,9 +1,6 @@
 import {
   Component,
-  ElementRef,
   OnInit,
-  Renderer2,
-  ViewChild,
 } from '@angular/core';
 
 @Component({
@@ -12,13 +9,12 @@ import {
   styleUrls: ['./projectmanagement.component.scss'],
 })
 export class ProjectmanagementComponent implements OnInit {
-  @ViewChild('buttonsContainer', { static: true }) buttonsContainer: ElementRef;
 
-  constructor(private renderer: Renderer2) { }
-
-  ngOnInit(): void { }
+  constructor() { }
 
   public projects = [];
+
+  ngOnInit(): void { }
 
   generateButton() {
     this.projects.push({ name: 'Project ' + this.projects.length });
