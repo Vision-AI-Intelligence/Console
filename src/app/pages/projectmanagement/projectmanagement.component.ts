f96f932bcfa1904d4a292f965f1d5fc169313bf6
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-09-14 21:28:31 +0700

    fixed generateButton

--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -4,23 +4,23 @@ import {
   OnInit,
   Renderer2,
   ViewChild,
-} from "@angular/core";
+} from '@angular/core';
 
 @Component({
-  selector: "app-projectmanagement",
-  templateUrl: "./projectmanagement.component.html",
-  styleUrls: ["./projectmanagement.component.scss"],
+  selector: 'app-projectmanagement',
+  templateUrl: './projectmanagement.component.html',
+  styleUrls: ['./projectmanagement.component.scss'],
 })
 export class ProjectmanagementComponent implements OnInit {
-  @ViewChild("buttonsContainer", { static: true }) buttonsContainer: ElementRef;
+  @ViewChild('buttonsContainer', { static: true }) buttonsContainer: ElementRef;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   public projects = [];
 
   generateButton() {
-    this.projects.push({ name: "Project " + this.projects.length });
+    this.projects.push({ name: 'Project ' + this.projects.length });
   }
 }
