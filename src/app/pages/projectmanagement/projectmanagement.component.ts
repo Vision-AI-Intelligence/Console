8b74ab3baf0db18d4277f632baefc6e3fa7faf05
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-10-06 12:03:58 +0700

    Fix some bugs

--- src/app/pages/projectmanagement/projectmanagement.component.ts
+++ src/app/pages/projectmanagement/projectmanagement.component.ts
@@ -1,46 +1,51 @@
-import {
-  Component,
-  OnInit,
-} from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material';
-import { Router } from '@angular/router';
-import { CreateComponent } from 'src/app/components/dialogs/create/create.component';
-import { Project } from '../../models/project.model';
-import { ProjectService } from '../../services/project/project.service';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material";
+import { Router } from "@angular/router";
+import { CreateComponent } from "src/app/components/dialogs/create/create.component";
+import { Project } from "../../models/project.model";
+import { ProjectService } from "../../services/project/project.service";
 @Component({
-  selector: 'app-projectmanagement',
-  templateUrl: './projectmanagement.component.html',
-  styleUrls: ['./projectmanagement.component.scss'],
+  selector: "app-projectmanagement",
+  templateUrl: "./projectmanagement.component.html",
+  styleUrls: ["./projectmanagement.component.scss"],
 })
 export class ProjectmanagementComponent implements OnInit {
-  dialogWidth = '500px';
+  dialogWidth = "500px";
   constructor(
     private router: Router,
     public dialog: MatDialog,
-    private projectService: ProjectService,
-  ) { }
+    private projectService: ProjectService
+  ) {}
 
   public projects = [];
+  private dialogRef = null;
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   // generateButton() {
   //   this.projects.push({ name: 'Project ' + this.projects.length, id: '' + this.projects.length });
   // }
   async onCreate() {
     const inputData = {
-      pid: '000',
-      name: '000',
-      description: '000'
+      pid: "000",
+      name: "000",
+      description: "000",
     };
-    const dialogRef = this.dialog.open(CreateComponent, { width: this.dialogWidth, hasBackdrop: true, data: inputData });
-    dialogRef.afterClosed().subscribe((data) => {
-      this.projectService.createProject(data);
-      this.projects.push(data);
-      console.log(this.projects);
+
+    this.dialogRef = this.dialog.open(CreateComponent, {
+      width: this.dialogWidth,
+      hasBackdrop: true,
+      data: inputData,
+    });
+    this.dialogRef.afterClosed().subscribe((data) => {
+      if (data !== "" && data !== undefined && data !== null) {
+        this.projectService.createProject(data);
+        this.projects.push(data);
+        console.log(this.projects);
+      }
     });
   }
   async gotoEdit(proj) {
-    await this.router.navigate(['/editproject/' + proj.pid]);
+    await this.router.navigate(["/editproject/" + proj.pid + "/resources"]);
   }
 }
