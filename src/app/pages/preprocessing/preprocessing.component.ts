54158e16f04c3782c32324fe2159598f77d5d067
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-10-08 16:24:43 +0700

    preprocessing init

--- src/app/pages/preprocessing/preprocessing.component.ts
+++ src/app/pages/preprocessing/preprocessing.component.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreprocessingComponent implements OnInit {
 
+  selected = 'option2';
+  labels = [
+    {
+      name: 'Dog'
+    },
+    {
+      name: 'Cat'
+    },
+    {
+      name: 'Human face'
+    },
+    {
+      name: 'Dog'
+    },
+    {
+      name: 'Cat'
+    },
+    {
+      name: 'Human face'
+    }, {
+      name: 'Dog'
+    },
+    {
+      name: 'Cat'
+    },
+    {
+      name: 'Human face'
+    }, {
+      name: 'Dog'
+    },
+    {
+      name: 'Cat'
+    },
+    {
+      name: 'Human face'
+    },
+    {
+      name: 'Dog'
+    },
+    {
+      name: 'Cat'
+    },
+    {
+      name: 'Human face'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  deleteLabel() {
+    alert('Hello world');
+  }
+  addLabel(label: string) {
+    this.labels.push({
+      name: label
+    });
+  }
 }
