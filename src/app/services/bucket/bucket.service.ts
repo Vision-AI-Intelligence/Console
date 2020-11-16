ce909ee9a3d54b3f942d5a0acd47e87e5f3c0fbc
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-11-16 10:16:55 +0700

    added bucket functions and fixed [UI] sidenav

--- src/app/services/bucket/bucket.service.ts
+++ src/app/services/bucket/bucket.service.ts
@@ -33,4 +33,225 @@ export class BucketService {
       console.log('[CREATE] project ' + error);
     }
   }
+  async GetFilesNFoldersFromDirectory(projectId: string, bucketId: string, directory: string) {
+    try {
+      return await this.http.get(this.server.endpoint + 'bucket', {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId,
+          bid: bucketId,
+          d: directory
+        }
+      }).toPromise();
+    } catch (e) {
+      console.log('[GET] bucket -> files n folders ' + e);
+    }
+  }
+
+  // check file's | folder's information
+  async GetMetadataFromFileOrFolder(projectId: string, bucketId: string, nameOfItem: string) {
+    try {
+      return await this.http.get(this.server.endpoint + 'bucket/metadata', {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId,
+          bid: bucketId,
+          f: nameOfItem
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[GET] bucket -> metadata' + error);
+    }
+  }
+
+  // view config of bucket
+  async GetBucketOptions(projectId: string, bucketId: string) {
+    try {
+      return await this.http.get(this.server.endpoint + 'bucket/options', {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId,
+          bid: bucketId
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[GET] bucket -> options ' + error);
+    }
+  }
+
+  // modify bucket's options
+  async ModifyBucketOptions(projectId: string, bucketId: string, opts: Array<any>) {
+    try {
+      return await this.http.post(this.server.endpoint + 'bucket/options', {
+        pid: projectId,
+        bid: bucketId,
+        options: opts
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[UPDATE] bucket -> options ' + error);
+    }
+  }
+  async UploadFile(file: FormData, projectId: string, bucketId: string, directory: string) {
+    try {
+      return await this.http.post(this.server.endpoint + 'bucket/upload', file, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId,
+          bid: bucketId,
+          d: directory
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[POST] bucket -> upload file ' + error);
+    }
+  }
+
+  async MakeNewDirectory(projectId: string, bucketId: string, directoryName: string, currentDirectory: string) {
+    try {
+      return this.http.post(this.server.endpoint + 'bucket/mkdir', {
+        pid: projectId,
+        bid: bucketId,
+        dir: directoryName,
+        d: currentDirectory
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[POST] bucket -> make new dir ' + error);
+    }
+  }
+  async MoveFileOrDirectory(projectId: string, bucketId: string, source: string, destination: string) {
+    try {
+      return await this.http.put(this.server.endpoint + 'bucket/mv', {
+        pid: projectId,
+        bid: bucketId,
+        src: source,
+        des: destination
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> move file|dir ' + error);
+    }
+  }
+  async CopyFileOrDirectory(projectId: string, bucketId: string, source: string, destination: string) {
+    try {
+      return await this.http.put(this.server.endpoint + 'bucket/cp', {
+        pid: projectId,
+        bid: bucketId,
+        src: source,
+        des: destination
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> copy file|directory ' + error);
+    }
+  }
+  async RemoveFileOrDirectory(projectId: string, bucketId: string, directory: string) {
+    try {
+      return await this.http.put(this.server.endpoint + 'bucket/rm', {
+        pid: projectId,
+        bid: bucketId,
+        d: directory,
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> remove file|directory ' + error);
+    }
+  }
+  async DownloadFileOrDirectory(projectId: string, bucketId: string, file: string) {
+    try {
+      return await this.http.get(this.server.endpoint + 'bucket/download', {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId,
+          bid: bucketId,
+          f: file
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> download file|directory ' + error);
+    }
+  }
+
+  async GetListOfJobs(projectId: string) {
+    try {
+      return await this.http.get(this.server.endpoint + 'bucket/jobs', {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        },
+        params: {
+          pid: projectId
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[GET] bucket -> get list of jobs ' + error);
+    }
+  }
+
+  async ZipFilesOrFolders(projectId: string, bucketId: string, source: string, destination: string) {
+    try {
+      return await this.http.put(this.server.endpoint + 'bucket/zip', {
+        pid: projectId,
+        bid: bucketId,
+        src: source,
+        des: destination
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> zip files|folders ' + error);
+    }
+  }
+
+  async UnzipFilesOrFolders(projectId: string, bucketId: string, source: string, destination: string) {
+    try {
+      return await this.http.put(this.server.endpoint + 'bucket/unzip', {
+        pid: projectId,
+        bid: bucketId,
+        src: source,
+        des: destination
+      }, {
+        headers: {
+          authorization: this.userAuthentication.idToken
+        }
+      }).toPromise();
+    } catch (error) {
+      console.log('[PUT] bucket -> unzip files|folders ' + error);
+    }
+  }
+
+  // current not available
+  async DownloadJob(projectId: string, bucketId: string, urlOfSource: string, destination: string) {
+    try {
+    } catch (error) {
+      console.log('[]');
+    }
+  }
 }
